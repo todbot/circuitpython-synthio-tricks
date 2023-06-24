541de1dc7b0b71854c11e30127e130659a6e8662
Tod Kurt <todbot@users.noreply.github.com>
2023-06-24 12:41:31 -0700

    first cut at monosynth1 port

--- examples/monosynth1/code.py
+++ examples/monosynth1/code.py
@@ -2,13 +2,15 @@
 # 22 Jun 2023 - @todbot / Tod Kurt
 # part of https://github.com/todbot/circuitpython-synthio-tricks
 #
-# USB MIDI control:
-# - note on / off as expected
-# - CC 74 - filter cutoff
-# - CC 71 - filter resonance
-# - CC 72 - amp envelope release time
-# - CC 1 - (mod wheel) - pitch vibrato
+# Responds to these USB MIDI messages:
+# - note on / off
+# - filter cutoff      -- CC 74
+# - filter resonance   -- CC 71
+# - env release time   -- CC 72 / CC 18
+# - osc detune amount  -- CC 93
+# - pitch vibrato      -- CC 1 (modwheel)
 #
+
 import time,random
 import board
 import audiomixer, audiopwmio
@@ -23,12 +25,12 @@ from adafruit_midi.control_change import ControlChange
 import neopixel   # circup install neopixel
 
 oscs_per_note = 3      # how many oscillators for each note
-osc_detune = 0.01      # how much to detune oscillators for phatness
+osc_detune = 0.001     # how much to detune oscillators for phatness
 filter_freq_lo = 100   # filter lowest freq
 filter_freq_hi = 4500  # filter highest freq
 filter_res_lo = 0.5    # filter q lowest value
 filter_res_hi = 2.0    # filter q highest value
-vibrato_lfo_hi = 0.5   # vibrato amount when modwheel is maxxed out
+vibrato_lfo_hi = 0.1   # vibrato amount when modwheel is maxxed out
 vibrato_rate = 5       # vibrato frequency
 
 led = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
@@ -40,14 +42,14 @@ mixer = audiomixer.Mixer(channel_count=1, sample_rate=28000, buffer_size=2048)
 synth = synthio.Synthesizer(channel_count=1, sample_rate=28000)
 audio.play(mixer)
 mixer.voice[0].play(synth)
-mixer.voice[0].level = 0.8
+mixer.voice[0].level = 0.75
 
-# our oscillator waveform, a 512 sample downward saw wave going from +/-30k
-wave_saw = np.linspace(30000, -30000, num=512, dtype=np.int16)  # max is +/-32k but gives us headroom
-lfo_vibrato = synthio.LFO(rate=vibrato_rate, scale=0.31) # scale set with modwheel
+# our oscillator waveform, a 512 sample downward saw wave going from +/-28k
+wave_saw = np.linspace(28000, -28000, num=512, dtype=np.int16)  # max is +/-32k but gives us headroom
+lfo_vibrato = synthio.LFO(rate=vibrato_rate, scale=0.01 ) # scale set with modwheel
 
 oscs = []   # holds currently sounding oscillators
-filter_freq = 1000  # current setting of filter
+filter_freq = 2000  # current setting of filter
 filter_res = 1.0    # current setting of filter
 amp_env_release_time = 0.8  # current release time
 note_played = 0  # current note playing
@@ -57,9 +59,10 @@ def map_range(s, a1, a2, b1, b2): return  b1 + ((s - a1) * (b2 - b1) / (a2 - a1)
 
 # midi note on
 def note_on(notenum, vel):
+    amp_level = map_range(vel, 0,127, 0,1)
     amp_env = synthio.Envelope(attack_time=0.1, decay_time=0.05,
                                release_time=amp_env_release_time,
-                               attack_level=1, sustain_level=0.8)
+                               attack_level=amp_level, sustain_level=amp_level*0.8)
     f = synthio.midi_to_hz(notenum)
     oscs.clear()  # chuck out old oscs to make new ones
     for i in range(oscs_per_note):
@@ -106,5 +109,7 @@ while True:
             filter_freq = map_range( msg.value, 0,127, filter_freq_lo, filter_freq_hi)
         elif msg.control == 71: # filter resonance
             filter_res = map_range( msg.value, 0,127, filter_res_lo, filter_res_hi)
-        elif msg.control == 72: # env release time
-            amp_env_release_time = map_range( msg.value, 0,127, 0.1, 5)
+        elif msg.control == 72 or msg.control == 18: # env release time
+            amp_env_release_time = map_range( msg.value, 0,127, 0.1, 1)
+        elif msg.control == 93:  # 'chorus' amount (detune amount)
+            osc_detune = map_range( msg.value, 0,127, 0, 0.01)
