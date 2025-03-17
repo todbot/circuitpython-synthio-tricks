5b32052e4deeabcd59054a8010fbbd6857ca22d9
Tod Kurt <todbot@users.noreply.github.com>
2025-03-17 15:13:47 -0700

    fix tiny_lfo_song since synthio.LFO ranges -1/+1 now

--- examples/tiny_lfo_song/code.py
+++ examples/tiny_lfo_song/code.py
@@ -3,16 +3,19 @@
 # video demo: https://www.youtube.com/watch?v=m_ALNCWXor0
 # requires CircuitPython 8.2.0-beta0 or later
 import board, time, audiopwmio, synthio, random
+import ulab.numpy as np
 audio = audiopwmio.PWMAudioOut(board.GP10)
 synth = synthio.Synthesizer(sample_rate=22050)
 audio.play(synth)
 
 synth.envelope = synthio.Envelope(attack_time=0.1, release_time=0.05)
 
-lfo_tremo1 = synthio.LFO(rate=3,    offset=0.5, scale=0.5)  # 3 Hz for fastest note
-lfo_tremo2 = synthio.LFO(rate=2,    offset=0.5, scale=0.5)  # 2 Hz for middle note
-lfo_tremo3 = synthio.LFO(rate=1,    offset=0.5, scale=0.5)  # 1 Hz for lower note
-lfo_tremo4 = synthio.LFO(rate=0.75, offset=0.5, scale=0.5)  # 0.75 Hz for lowest bass note
+lfo_wav = np.array([0,32000,0], dtype=np.int16)
+lfo_tremo1 = synthio.LFO(rate=3,    waveform=lfo_wav)  # 3 Hz for fastest note
+lfo_tremo2 = synthio.LFO(rate=2,    waveform=lfo_wav)  # 2 Hz for middle note
+lfo_tremo3 = synthio.LFO(rate=1,    waveform=lfo_wav)  # 1 Hz for lower note
+lfo_tremo4 = synthio.LFO(rate=0.75, waveform=lfo_wav)  # 0.75 Hz for lowest bass note
+
 
 def do_notes(midi_note):
     note1 = synthio.Note( synthio.midi_to_hz(midi_note), amplitude=lfo_tremo1)
